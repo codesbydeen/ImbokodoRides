c0cb82761f41e2098d0ae717380658abf8606dae
Geraldine <c4dn5wld3@vossie.net>
2023-05-09 15:57:03 +0200

    Building Onboarding-Screen

--- App.js
+++ App.js
@@ -1,20 +1,25 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import LoginScreen from "./screens/LoginScreen";
+import OnboardingScreen from "./screens/OnboardingScreen";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Onboarding">
+        <Stack.Screen
+          name="Onboarding"
+          component={OnboardingScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Login"
+          component={LoginScreen}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
---
+++ screens/OnboardingScreen.js
@@ -0,0 +1,81 @@
+import React from "react";
+import { View, Text, Image, TouchableOpacity } from "react-native";
+import Onboarding from "react-native-onboarding-swiper";
+
+const Dots = ({ selected }) => {
+  let backgroundColor;
+  backgroundColor = selected ? "#673AB7" : "#808080";
+  return (
+    <View
+      style={{
+        height: 5,
+        width: 5,
+        marginHorizontal: 3,
+        backgroundColor,
+      }}
+    />
+  );
+};
+
+const Done = ({ ...props }) => (
+  <TouchableOpacity
+    style={{
+      marginRight: 12,
+    }}
+    {...props}
+  >
+    <Text style={{ color: "#673AB7" }}>Done</Text>
+  </TouchableOpacity>
+);
+
+const OnboardingScreen = ({ navigation }) => {
+  return (
+    <Onboarding
+      onSkip={() => navigation.navigate("Login")}
+      onDone={() => navigation.navigate("Login")}
+      DotComponent={Dots}
+      DoneButtonComponent={Done}
+      bottomBarColor="#ffffff"
+      pages={[
+        {
+          backgroundColor: "#fff",
+          image: (
+            <Image
+              source={require("./assets/onboarding1.png")}
+              style={{ width: 200, height: 200 }}
+            />
+          ),
+          title: "Welcome to Imbokodo Rides!",
+          subtitle:
+            "Book a ride just like a taxi, to take you wherever you want to go",
+        },
+        {
+          backgroundColor: "#fff",
+          image: (
+            <Image
+              source={require("./assets/onboarding2.png")}
+              style={{ width: 200, height: 200 }}
+            />
+          ),
+          title: "Ordering Rides Has Been Made Easy",
+          subtitle:
+            "Book rides anytime by matching riders with divers within the area",
+        },
+        {
+          backgroundColor: "#fff",
+          image: (
+            <Image
+              source={require("./assets/onboarding3.png")}
+              style={{ width: 200, height: 200 }}
+            />
+          ),
+          title: "Order Your Ride Now",
+          subtitle:
+            "Imbokodo Rides has made booking rides easy, affordable and reliable",
+        },
+      ]}
+    />
+  );
+};
+
+export default OnboardingScreen;
