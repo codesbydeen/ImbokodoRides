ce2de0411c363027ec0ec1f8ef1bf7af4182010f
Maria <3bb9ts3r4@vossie.net>
2023-05-10 18:03:31 +0200

    For user input

---
+++ Components/CustomInput/CustomInput.js
@@ -0,0 +1,12 @@
+import React from "react"
+import {View, Text} from "react-native"
+
+const CustomInput = () => {
+    return(
+        <View>
+            <Text>Custom Input</Text>
+        </View>
+    )
+}
+
+export default CustomInput
