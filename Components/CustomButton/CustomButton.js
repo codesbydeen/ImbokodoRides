bf4c56dc968d330006bbe989589768757d4f89a7
Maria <3bb9ts3r4@vossie.net>
2023-05-10 18:51:19 +0200

    Update

--- Components/CustomButton/CustomButton.js
+++ Components/CustomButton/CustomButton.js
@@ -1,3 +1,11 @@
 import React from "react"
 import {View, Text} from "react-native"
 
+const CustomButton = () => {
+    return(
+        <View>
+            <Text>Button</Text>
+        </View>
+    )
+}
+export default CustomButton
