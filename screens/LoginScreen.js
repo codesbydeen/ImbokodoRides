f8c3e8535445db09e835c0da7e2a4900e473a854
Maria <3bb9ts3r4@vossie.net>
2023-05-10 18:03:26 +0200

    Update for login screen

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,15 +1,19 @@
 import React from "react";
-import { View, Text, Button } from "react-native";
+import { View, Text, Button, TextInput} from "react-native";
+import CustomInput from "../Components/CustomInput/CustomInput";
 
 
 function LoginScreen() {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Login Screen</Text>
+    
+
       <Button
       title= "Continue"
       onPress={() => navigation.navigate("Sign Up")}
     />
+
+    <CustomInput/>
     </View>
   );
 
