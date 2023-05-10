370f2b6e5f80d74587e2b0ffdb167c997e6f2ccc
Maria <3bb9ts3r4@vossie.net>
2023-05-10 16:39:14 +0200

    Undated

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,12 +1,21 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Button } from "react-native";
+
 
 function LoginScreen() {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <Text>Login Screen</Text>
+      <Button
+      title= "Continue"
+      onPress={() => navigation.navigate("Sign Up")}
+    />
     </View>
   );
-}
+
+ 
+
+};
+
 
 export default LoginScreen;
