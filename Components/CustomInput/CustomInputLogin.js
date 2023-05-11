24fbbf92da5907b972e7f3baf49971c12cf81924
Maria <3bb9ts3r4@vossie.net>
2023-05-11 13:44:22 +0200

    Update

--- Components/CustomInput/CustomInputLogin.js
+++ Components/CustomInput/CustomInputLogin.js
@@ -3,12 +3,15 @@ import {View, Text, TextInput, StyleSheet} from "react-native"
 
 const CustomInputLogin = ({value, setValue, placeholder, secureTextEntry}) => {
     return(
-        <View>
-            
+        <View style = {styles.container}>
+            <TextInput placeholder = "placeholder" style = {styles.input}/>
         </View>
     );
 };
-
+ const styles = StyleSheet.create({
+    container: {},
+    input: {},
+ });
 
 
 export default CustomInputLogin;
--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -12,4 +12,8 @@ const LoginScreen = () => {
     );
 };
 
+
+
+
+
 export default LoginScreen;
