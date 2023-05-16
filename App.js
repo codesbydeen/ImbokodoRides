c8781921205d9c906a096a51028d250b644330a9
Maria <3bb9ts3r4@vossie.net>
2023-05-16 22:14:20 +0200

    From LoginScreen

--- App.js
+++ App.js
@@ -1,25 +1,64 @@
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import LoginScreen from "./screens/LoginScreen";
-import OnboardingScreen from "./screens/OnboardingScreen";
+import React from "react"
+import {View, Text, StyleSheet} from "react-native"
+import CustomInput from "../Components/CustomInput/CustomInputLogin"
+import CustomButton from "../Components/CustomButton/CustomButtonLogin";
 
-const Stack = createNativeStackNavigator();
+const LoginScreen = () => {
+    return(
+        <View>
+            
+            <Text style = {styles.title1}>Login</Text>
 
-export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Onboarding.">
-        <Stack.Screen
-          name="Onboarding"
-          component={OnboardingScreen}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="Login"
-          component={LoginScreen}
-          options={{ headerShown: false }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-}
+        <Text style = {styles.text1}>Welcome back!</Text>
+        <Text style = {styles.text3}>Let's get you logged In</Text>
+        <Text style = {styles.text2}>Enter in your phone number below:</Text>
+            <CustomInput/>
+
+            <CustomButton/>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    root:{
+      flex: 1,
+      backgroundColor: "#ffffff"
+    },
+    title1:{
+      fontSize: 30,
+      fontWeight: "bold",
+      fontColor: "#051C60",
+      margin : 10,
+      textAlign: "center",
+    },
+
+    text1:{
+        fontSize: 25,
+        fontWeight: "600",
+        textAlign: "center",
+        margin: 5,
+    },
+
+    text2: {
+        textAlign: "center",
+        fontWeight: "400",
+        fontSize: 20,
+        margin: 5,
+    },
+
+    text3: {
+        fontSize: 20,
+        textAlign: "center",
+        margin: 10,
+    }
+
+
+
+
+
+
+});
+
+
+
+export default LoginScreen;
