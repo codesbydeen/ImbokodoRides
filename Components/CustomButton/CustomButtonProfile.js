18370b761623250f254216fafbe814ab4cf2a4a2
Maria <3bb9ts3r4@vossie.net>
2023-05-24 23:37:28 +0200

    Update

---
+++ Components/CustomButton/CustomButtonProfile.js
@@ -0,0 +1,49 @@
+import React from "react"
+import {View, Text, StyleSheet, Pressable} from "react-native"
+
+const CustomButtonProfile = ({onPress, text}) => {
+    return(
+        <Pressable onPress = {onPress} 
+        style = {[styles.container,
+            
+        
+        
+        ]}>
+
+            <Text style = {[
+                styles.text,
+                
+            ]}>
+                {text}
+            </Text>
+
+            
+        </Pressable>
+
+
+
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: "#800080",
+
+        width: "75%",
+        padding: 15,
+        marginVertical: 5,
+        alignItems: "center",
+        alignSelf: "center",
+        borderRadius: 5,
+        
+    },
+    text: {
+        fontWeight: "bold",
+        color:"white",
+        fontSize: 20,
+    },
+
+
+});
+
+export default CustomButtonProfile;
---
+++ screens/ProfileScreen.js
@@ -0,0 +1,78 @@
+import React, {useState} from "react";
+import { View, Text, StyleSheet, TextInput} from "react-native";
+import CustomInput from "../Components/CustomInput/CustomInputRegistration";
+import CustomButton from "../Components/CustomButton/CustomButton";
+
+const ProfileScreen = () => {
+  const [full_name, setFull_name] = useState(" ");
+  const [Phone_number, setPhone_number] = useState(" ");
+  const [Email_address, setEmail_address] = useState(" ");
+
+
+
+  const onSavePress = () => {
+    console.warn("Saved");
+  }
+
+
+  return (
+    <View style={{ flex: 1, justifyContent: "center" }}>
+      <Text style = {styles.title}>Profile</Text>
+    
+ 
+
+    <CustomInput 
+    placeholder= "Full name" 
+    value = {full_name} 
+    setValue={setFull_name} 
+    />
+
+<CustomInput 
+    placeholder= "Phone number" 
+    value = {Phone_number} 
+    setValue={setPhone_number} 
+    />
+
+<CustomInput 
+    placeholder= "Email address" 
+    value = {Email_address} 
+    setValue={setEmail_address} 
+    />
+   
+    <CustomButton 
+    text = "SAVE" 
+    onPress={ onSavePress}/>
+    
+    </View>
+    
+    );
+     
+};
+
+const styles = StyleSheet.create({
+  root:{
+    flex: 1,
+    backgroundColor: "#ffffff"
+  },
+  title:{
+    fontSize: 24,
+    fontWeight: "bold",
+    fontColor: "#051C60",
+    margin : 10,
+    textAlign: "center",
+  },
+
+text:{
+  fontSize:20,
+  fontWeight: "bold",
+  textAlign: "center",
+  fontColor: "black",
+
+},
+  
+}
+
+);
+
+
+export default ProfileScreen;
