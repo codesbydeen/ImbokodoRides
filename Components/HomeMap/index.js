04e6d36fd025e099ff6ae5501908e291f1d93e0f
Geraldine <c4dn5wld3@vossie.net>
2023-05-27 13:20:45 +0200

    Render cars on map

--- Components/HomeMap/index.js
+++ Components/HomeMap/index.js
@@ -1,32 +1,41 @@
 import React from "react";
 import { Flatlist, Image } from "react-native";
 import MapView, { PROVIDER_GOOGLE, Marker } from "react-native-maps";
-import cars from '../../assets/data/cars';
+import cars from "../../assets/data/cars";
 
 const HomeMap = () => {
+  const getImage = (type) => {
+    if (type === "RideX") {
+      return require("../../assets/images/top-RideX.png");
+    }
+    if (type === "RideComfort") {
+      return require("../../assets/images/top-RideComfort.png");
+    }
+    return require("../../assets/images/top-RideXL.png");
+  };
+
   return (
     <MapView
       style={{ width: "100%", height: "100%" }}
       provider={PROVIDER_GOOGLE}
       initialRegion={{
-        latitude: 37.78825,
-        longitude: -122.4324,
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421,
+        latitude: 28.450627,
+        longitude: -16.263045,
+        latitudeDelta: 0.0222,
+        longitudeDelta: 0.0121,
       }}
     >
-
-      <Flatlist 
-      data={cars}
-      renderItem={}
-      />
-
-      <Marker coordinate={{ latitude: 37.78825, longitude: -122.4324 }}>
-        <Image
-          style={{ width: 50, height: 50, resizeMode: "contain" }}
-          source={require("../../assets/images/top-RideX.png")}
-        />
-      </Marker>
+      {cars.map((car) => (
+        <Marker
+          key={car.id}
+          coordinate={{ latitude: car.latitude, longitude: car.longitude }}
+        >
+          <Image
+            style={{ width: 50, height: 50, resizeMode: "contain" }}
+            source={getImage(car.type)}
+          />
+        </Marker>
+      ))}
     </MapView>
   );
 };
