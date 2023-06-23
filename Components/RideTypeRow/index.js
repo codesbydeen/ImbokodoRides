9193bf003151574179f0b1970e4f90079452c2b4
Geraldine <c4dn5wld3@vossie.net>
2023-06-23 10:45:41 +0200

    Navigation Home

--- Components/RideTypeRow/index.js
+++ Components/RideTypeRow/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Image, Text, View } from "react-native";
+import { Image, Text, View, ScrollView } from "react-native";
 import styles from "./styles";
 import { Ionicons } from "@expo/vector-icons";
 
@@ -17,24 +17,26 @@ const RideTypeRow = (props) => {
     return require("../../assets/images/RideXL.jpeg");
   };
   return (
-    <View style={styles.container}>
-      {/* Image */}
-      <Image style={styles.image} source={getImage()} />
+    <ScrollView>
+      <View style={styles.container}>
+        {/* Image */}
+        <Image style={styles.image} source={getImage()} />
 
-      {/* Ride information */}
-      <View style={styles.middleContainer}>
-        <Text style={styles.type}>
-          {type.type} <Ionicons name="person" size={16} color="black" />4
-        </Text>
-        <Text style={styles.time}>10:45AM arrival time</Text>
-      </View>
+        {/* Ride information */}
+        <View style={styles.middleContainer}>
+          <Text style={styles.type}>
+            {type.type} <Ionicons name="person" size={16} color="black" />4
+          </Text>
+          <Text style={styles.time}>10:45AM arrival time</Text>
+        </View>
 
-      {/* Display ride price */}
-      <View style={styles.rightContainer}>
-        <Ionicons name="pricetag" size={12} color={"#42d742"} />
-        <Text style={styles.price}>est. R{type.price}</Text>
+        {/* Display ride price */}
+        <View style={styles.rightContainer}>
+          <Ionicons name="pricetag" size={12} color={"#42d742"} />
+          <Text style={styles.price}>est. R{type.price}</Text>
+        </View>
       </View>
-    </View>
+    </ScrollView>
   );
 };
 
--- Components/RideTypeRow/styles.js
+++ Components/RideTypeRow/styles.js
@@ -5,7 +5,7 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "space-between",
     alignItems: "center",
-    padding: 20,
+    padding: 15,
   },
   image: {
     height: 70,
