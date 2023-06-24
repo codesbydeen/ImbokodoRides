335c83475def6d760262948f56f364ce5ae5bf2b
Geraldine <c4dn5wld3@vossie.net>
2023-06-24 17:45:32 +0200

    Install Maps Direction

--- DriverApp/screens/HomeScreen/index.js
+++ DriverApp/screens/HomeScreen/index.js
@@ -1,10 +1,15 @@
 import React from "react";
 import { View } from "react-native";
 import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
+import MapViewDirections from "react-native-maps-directions";
+
+const origin = { latitude: 37.3318456, longitude: -122.0296002 };
+const destination = { latitude: 37.771707, longitude: -122.4053769 };
+const GOOGLE_MAPS_APIKEY = "AIzaSyDZ5FH-agV2WV5I5FdAQkDZ_GcrHQR45Ws";
 
 const HomeScreen = () => {
   return (
-    <View style={{ width: 100, height: 100 }}>
+    <View>
       <MapView
         style={{ width: "100%", height: "100%" }}
         provider={PROVIDER_GOOGLE}
@@ -14,7 +19,13 @@ const HomeScreen = () => {
           latitudeDelta: 0.0922,
           longitudeDelta: 0.0421,
         }}
-      />
+      >
+        <MapViewDirections
+          origin={origin}
+          destination={destination}
+          apikey={GOOGLE_MAPS_APIKEY}
+        />
+      </MapView>
     </View>
   );
 };
