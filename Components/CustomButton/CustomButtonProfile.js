181134e5ee8f1870c3da92ed09d309775f1fd325
Geraldine <c4dn5wld3@vossie.net>
2023-06-25 11:15:26 +0200

    update branch

--- Components/CustomButton/CustomButtonProfile.js
+++ Components/CustomButton/CustomButtonProfile.js
@@ -1,53 +1,36 @@
-import React from "react"
-import {View, Text, StyleSheet, Pressable} from "react-native"
+import React from "react";
+import { View, Text, StyleSheet, Pressable } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 
-const CustomButtonProfile = ({onPress, text}) => {
-    const nav = useNavigation();
+const CustomButtonProfile = ({ onPress, text }) => {
+  const nav = useNavigation();
 
-    return(
-        <Pressable onPress = {() => nav.navigate("Ridehistory")} 
-        style = {[styles.container,
-            
-        
-        
-        ]}>
-
-            <Text style = {[
-                styles.text,
-                
-            ]}>
-                {text}
-            </Text>
-
-            
-        </Pressable>
-
-
-
-    );
+  return (
+    <Pressable
+      onPress={() => nav.navigate("Ridehistory")}
+      style={[styles.container]}
+    >
+      <Text style={[styles.text]}>{text}</Text>
+    </Pressable>
+  );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        backgroundColor: "#800080",
+  container: {
+    backgroundColor: "#800080",
 
-        width: "65%",
-        padding: 15,
-        marginVertical: 5,
-        alignItems: "center",
-        alignSelf: "center",
-        borderRadius: 5,
-        
-    },
-    text: {
-        fontWeight: "bold",
-        color:"white",
-        fontSize: 15,
-        
-    },
-
-
+    width: "65%",
+    padding: 15,
+    marginVertical: 5,
+    alignItems: "center",
+    alignSelf: "center",
+    borderRadius: 5,
+  },
+  text: {
+    fontWeight: "bold",
+    color: "white",
+    fontSize: 15,
+  },
 });
 
 export default CustomButtonProfile;
--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,5 +1,5 @@
-import React, {useState} from "react";
-import { View, Text, StyleSheet, TextInput} from "react-native";
+import React, { useState } from "react";
+import { View, Text, StyleSheet, TextInput } from "react-native";
 import CustomInput from "../Components/CustomInput/CustomInputProfile";
 import CustomButton from "../Components/CustomButton/CustomButtonProfile";
 
@@ -10,69 +10,57 @@ const ProfileScreen = () => {
 
   const onSavePress = () => {
     console.warn("Saved");
-  }
-
+  };
 
   return (
     <View style={{ flex: 1, justifyContent: "center" }}>
-      <Text style = {styles.title}>Profile</Text>
-    
- 
-      <Text>              Full name</Text>
-    <CustomInput 
-    placeholder= "Full name" 
-    value = {full_name} 
-    setValue={setFull_name} 
-    />
+      <Text style={styles.title}>Profile</Text>
 
-      <Text>              Phone number</Text>
-<CustomInput 
-    placeholder= "Phone number" 
-    value = {Phone_number} 
-    setValue={setPhone_number} 
-    />
+      <Text> Full name</Text>
+      <CustomInput
+        placeholder="Full name"
+        value={full_name}
+        setValue={setFull_name}
+      />
 
-      <Text>              Email Address</Text>
-<CustomInput 
-    placeholder= "Email address" 
-    value = {Email_address} 
-    setValue={setEmail_address} 
-    />
-   
-    <CustomButton 
-    text = "SAVE" 
-    onPress={ onSavePress}/>
-    
+      <Text> Phone number</Text>
+      <CustomInput
+        placeholder="Phone number"
+        value={Phone_number}
+        setValue={setPhone_number}
+      />
+
+      <Text> Email Address</Text>
+      <CustomInput
+        placeholder="Email address"
+        value={Email_address}
+        setValue={setEmail_address}
+      />
+
+      <CustomButton text="SAVE" onPress={onSavePress} />
     </View>
-    
-    );
-     
+  );
 };
 
 const styles = StyleSheet.create({
-  root:{
+  root: {
     flex: 1,
-    backgroundColor: "#ffffff"
+    backgroundColor: "#ffffff",
   },
-  title:{
+  title: {
     fontSize: 24,
     fontWeight: "bold",
     fontColor: "#051C60",
-    margin : 10,
+    margin: 10,
     textAlign: "center",
   },
 
-text:{
-  fontSize:20,
-  fontWeight: "bold",
-  textAlign: "center",
-  fontColor: "black",
-
-},
-  
-}
-
-);
-
+  text: {
+    fontSize: 20,
+    fontWeight: "bold",
+    textAlign: "center",
+    fontColor: "black",
+  },
+});
 
 export default ProfileScreen;
