f22a0e57c8eb30d5b95bfc86c2b63b10d2bc79ec
Geraldine <c4dn5wld3@vossie.net>
2023-06-25 10:42:03 +0200

    Create Home Screen

--- DriverApp/screens/HomeScreen/index.js
+++ DriverApp/screens/HomeScreen/index.js
@@ -1,7 +1,9 @@
 import React from "react";
-import { View } from "react-native";
+import { View, Text, Dimensions, Pressable } from "react-native";
 import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
 import MapViewDirections from "react-native-maps-directions";
+import { Entypo, AntDesign } from "@expo/vector-icons";
+import styles from "./styles.js";
 
 const origin = { latitude: 37.3318456, longitude: -122.0296002 };
 const destination = { latitude: 37.771707, longitude: -122.4053769 };
@@ -11,13 +13,14 @@ const HomeScreen = () => {
   return (
     <View>
       <MapView
-        style={{ width: "100%", height: "100%" }}
+        style={{ width: "100%", height: Dimensions.get("window").height - 90 }}
         provider={PROVIDER_GOOGLE}
+        showsUserLocation={true}
         initialRegion={{
-          latitude: 37.78825,
-          longitude: -122.4324,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
+          latitude: -26.02084,
+          longitude: 28.13356,
+          latitudeDelta: 0.0222,
+          longitudeDelta: 0.0121,
         }}
       >
         <MapViewDirections
@@ -26,6 +29,16 @@ const HomeScreen = () => {
           apikey={GOOGLE_MAPS_APIKEY}
         />
       </MapView>
+      {styles.roundButton}
+      <Pressable onPress={() => console.warn("Hey")}>
+        <Entypo name="menu" size={24} color="#4a4a4a" />
+      </Pressable>
+
+      <View style={styles.bottomContainer}>
+        <AntDesign name="questioncircle" size={24} color="#4a4a4a" />
+        <Text style={styles.bottomText}>You are offline</Text>
+        <Entypo name="menu" size={24} color="#4a4a4a" />
+      </View>
     </View>
   );
 };
---
+++ DriverApp/screens/HomeScreen/styles.js
@@ -0,0 +1,18 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  bottomContainer: {
+    height: 100,
+    backgroundColor: "white",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    padding: 15,
+  },
+  bottomText: {
+    fontSize: 22,
+    color: "#4a4a4a",
+  },
+});
+
+export default styles;
